c22eb7b149970e4d18fdacf96bd4a9e358d88522
syj110357 <yus298@ucsd.edu>
2019-02-04 16:27:39 -0800

    first push

---
+++ Assets/scripts/Camera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    public GameObject player;
+    public float xmin;
+    public float xmax;
+    public float ymin;
+    public float ymax;
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        float x = Mathf.Clamp(player.transform.position.x, xmin, xmax);
+        float y = Mathf.Clamp(player.transform.position.y, ymin, ymax);
+        gameObject.transform.position = new Vector3(x, y, gameObject.transform.position.z);
+    }
+}
---
+++ Assets/scripts/PlayerMove.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    private float speed = 10f;
+    private float jumpheight = 1500f;
+    private float moveX;
+    private bool isground = false;
+    private bool faceRight = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Playermove();
+        jumpcheck();
+	}
+    void Playermove()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        { 
+            if (Input.GetKey(KeyCode.S))
+            {
+                downJump();
+            }
+            else jump();
+        }
+        /*else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump();
+        }*/
+        moveX = Input.GetAxis("Horizontal");
+        if (moveX < 0f && faceRight==true)
+        {
+            flipPlayer();
+        }
+        if (moveX > 0f && faceRight == false)
+        {
+            flipPlayer();
+        }
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * speed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+    void flipPlayer()
+    {
+        Vector3 scale= new Vector3(-gameObject.transform.localScale.x, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
+        gameObject.transform.localScale = scale;
+        faceRight = !faceRight;
+    }
+    void jump()
+    {
+        if (isground)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpheight);
+            isground = false;
+        }
+    }
+    void downJump()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, Vector2.down, 1.0f, 1 << LayerMask.NameToLayer("floor"));
+        if (hit)
+        {
+            hit.collider.isTrigger = true;
+        }
+        isground = false;
+    }
+    void jumpcheck()
+    {
+        if (!isground && gameObject.GetComponent<Rigidbody2D>().velocity.y>0)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(this.transform.position, Vector2.up, 1.0f, 1 << LayerMask.NameToLayer("floor"));
+            if (hit)
+            {
+                hit.collider.isTrigger = true;
+            }
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag=="ground")
+        {
+            if (collision.gameObject.transform.position.y < gameObject.transform.position.y)
+            {
+                isground = true;
+                //collision.gameObject.GetComponent<BoxCollider2D>().isTrigger = false;
+            }
+            else
+            {
+                //collision.gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
+                //Debug.Log("HIIIIIIIIIIIIIIT");
+            }
+        }    
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        collision.gameObject.GetComponent<BoxCollider2D>().isTrigger = false;
+        /*if (collision.gameObject.transform.position.y < gameObject.transform.position.y)
+        {
+            collision.gameObject.GetComponent<BoxCollider2D>().isTrigger = false;
+        }*/
+            
+    }
+    
+}
