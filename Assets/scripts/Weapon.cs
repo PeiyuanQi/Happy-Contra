905e13a2b747e563a42c3865adce2cd021dd5a04
peiyuanqi <qipeiyuan@outlook.com>
2019-02-11 10:58:46 -0800

    rename shootRate, add first bullet when first short press fire1

--- Assets/scripts/Weapon.cs
+++ Assets/scripts/Weapon.cs
@@ -6,7 +6,7 @@ public class Weapon : MonoBehaviour {
 
     public Transform firePoint;
     public GameObject bulletPrefab;
-    public float shootSpeed = 5;  //表示每秒发射子弹的个数 俗称子弹的发射速率
+    public float shootRate = 5;  //表示每秒发射子弹的个数 俗称子弹的发射速率
 
     private float shootTimer = 0;  //表示子弹的生成时间间隔 用来控制子弹的发射间隔
 
@@ -14,15 +14,11 @@ public class Weapon : MonoBehaviour {
 
     private void Start()
     {
-        shootTimerInterval = 1 / shootSpeed;
+        shootTimerInterval = 1 / shootRate;
     }
 
     // Update is called once per frame
     void Update () {
-		/*if (Input.GetButtonDown("Fire1"))
-        {
-            Shoot();
-        }*/
         if (Input.GetButton("Fire1"))
         {
             shootTimer += Time.deltaTime;  //让子弹的时间控制器不断加等时间间隔
@@ -31,6 +27,10 @@ public class Weapon : MonoBehaviour {
             {  //如果子弹发射的时间间隔超过时间控制器　　那么我们就发射子弹
 
                 shootTimer -= shootTimerInterval;  //让子弹的时间间隔回复到初始的情况下
+                Shoot();
+            }
+            else if (shootTimer - Time.deltaTime < 0.01f) //第一次短按也能发射子弹
+            {
                 Shoot();
             }
         }
