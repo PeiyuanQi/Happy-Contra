9694302be1d434784b41d826b9a4e63ddf7c3fe0
AoboAbbyLi <aobo0610@gmail.com>
2019-02-22 01:11:08 -0800

    update enemy with running enemy and flying enemy

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -22,7 +22,7 @@ public class Enemy : MonoBehaviour {
 
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnCollisionEnter2D(Collision2D other)
     {
         if (other.tag == "Player")
         {
@@ -31,15 +31,16 @@ public class Enemy : MonoBehaviour {
             {
                 playerLife.TakeDamage(damage);
             }
+            Destroy(gameObject);
         }
     }
 
-    void OnTriggerExit2D(Collider2D other)
+    void OnCollisionExit2D(Collision2D other)
     {
 
     }
 
-        public void TakeDamage (int damage)
+    public void TakeDamage (int damage)
     {
         health -= damage;
         if (health <= 0)
--- Assets/scripts/RunningEnemy.cs
+++ Assets/scripts/RunningEnemy.cs
@@ -2,23 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//向左移动的敌人
-public class FlyingEnemy : Enemy {
+public class RunningEnemy : Enemy {
 
     public float speed;
-    //private float currentposx;
-    //private float currentposy;
 
-    //public new int damage = 50;
-
-    void Start()
-    {
-        //currentposx = gameObject.transform.position.x;
+    // Use this for initialization
+    void Start () {
         speed = 0.04f;
     }
-
-    void Update()
-    {
+	
+	// Update is called once per frame
+	void Update () {
         transform.Translate(Vector2.left * speed);
         //如果在摄像机画面左侧 -> 消失
         if (Camera.Instance.LeftOutOfRange(gameObject.transform.position.x))
@@ -26,5 +20,4 @@ public class FlyingEnemy : Enemy {
             Destroy(gameObject);
         }
     }
-
 }
