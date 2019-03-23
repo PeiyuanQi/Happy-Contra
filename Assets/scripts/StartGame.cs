5a89c37f20fdfb386010fe78d94b27deb9db8b78
Sitan Gao <sitangao@usc.edu>
2019-03-22 18:28:55 -0700

    add main scene

---
+++ Assets/scripts/StartGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.gameObject.GetComponent<Button>().onClick.AddListener(delegate ()
+        {
+            StartCoroutine("Play");
+        });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator Play()
+    {
+        SceneManager.LoadScene("SampleScene");
+        yield return new WaitForSeconds(2);
+    }
+}
