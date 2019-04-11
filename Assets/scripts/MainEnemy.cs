8734fa9a57bbdd327b55f6f896b04c9f86a034ef
Wesley <736291679@qq.com>
2019-04-10 17:08:54 -0700

    Add a setting interface

--- Assets/scripts/MainEnemy.cs
+++ Assets/scripts/MainEnemy.cs
@@ -12,9 +12,9 @@ public class MainEnemy : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         transform.Translate(Vector3.right * Time.deltaTime , Space.World);
-        if(transform.localPosition.x > 800)
+        if(transform.localPosition.x > 500)
         {
-            Vector3 vector = new Vector3(-800, -200, 1);
+            Vector3 vector = new Vector3(-400, -150, 1);
             transform.localPosition = vector;
         }
     }
