e802c8b4cd0b905a680677c55593f0cf3c14aab6
peiyuanqi <qipeiyuan@outlook.com>
2019-04-18 12:57:41 -0700

    finish falling ground

---
+++ Assets/FallingGround.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingGround : MonoBehaviour {
+
+    private Rigidbody2D rg2d;
+    public float fallDelay;
+
+	// Use this for initialization
+	void Start () {
+        rg2d = GetComponent<Rigidbody2D>();
+	}
+
+    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            StartCoroutine(Fall());
+        }
+    }
+
+    IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(fallDelay);
+        rg2d.gravityScale = 1;
+        rg2d.mass = 1;
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
+        yield return 0;
+    }
+}
