b33890244c9e67bb661c5ce26f5b3d96ed4703a1
Lucas <hangdong.person@gmail.com>
2019-04-22 14:53:34 -0700

    fix triple bullet & hide portal
    fix triple bullet & hide portal

--- Assets/scripts/Bullet_S_down.cs
+++ Assets/scripts/Bullet_S_down.cs
@@ -37,16 +37,57 @@ public class Bullet_S_down : MonoBehaviour
     void OnTriggerEnter2D(Collider2D hitInfo)
     {
         Enemy enemy = hitInfo.GetComponent<Enemy>();
+        GameObject sound = GameObject.Find("Sound");
+        PlaySound play = sound.GetComponent<PlaySound>();
         if (enemy != null)
         {
+            play.PlayHitEnemy();
             enemy.TakeDamage(damage);
             Destroy(gameObject);
         }
 
-        PowerUpBlimp Blimp = hitInfo.GetComponent<PowerUpBlimp>();
-        if (Blimp != null)
+        PowerUpBlimp blimp = hitInfo.GetComponent<PowerUpBlimp>();
+        if (blimp != null)
         {
-            Blimp.TakeDamage(damage);
+            play.PlayHitBox();
+            blimp.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+        Box box = hitInfo.GetComponent<Box>();
+        if (box != null)
+        {
+            play.PlayHitBox();
+            box.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+        MonsterBlock mBlock = hitInfo.GetComponent<MonsterBlock>();
+        if (mBlock != null)
+        {
+            play.PlayHitBox();
+            mBlock.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+
+        Monster monster = hitInfo.GetComponent<Monster>();
+        if (monster != null)
+        {
+            play.PlayHitEnemy();
+            monster.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+
+        Turret turret = hitInfo.GetComponent<Turret>();
+        if (turret != null)
+        {
+            play.PlayHitEnemy();
+            turret.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+
+
+        if (hitInfo.gameObject.tag == "Destructable")
+        {
+            hitInfo.gameObject.GetComponent<DamageDestructable>().TakeDamage(damage);
             Destroy(gameObject);
         }
 
